d5bf3a7230d1060e3ef4af6fe4704b04912a9cdc
felixkrutov <googoldecaplex@bk.ru>
2025-09-30 16:37:00 +0300

    Update activate.fish

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -1,6 +1,4 @@
-
 function deactivate  -d "Exit virtual environment and return to normal shell environment"
-    # reset old environment variables
     if test -n "$_OLD_VIRTUAL_PATH"
         set -gx PATH $_OLD_VIRTUAL_PATH
         set -e _OLD_VIRTUAL_PATH
@@ -12,7 +10,6 @@ function deactivate  -d "Exit virtual environment and return to normal shell env
 
     if test -n "$_OLD_FISH_PROMPT_OVERRIDE"
         set -e _OLD_FISH_PROMPT_OVERRIDE
-        # prevents error when using nested fish instances (Issue #93858)
         if functions -q _old_fish_prompt
             functions -e fish_prompt
             functions -c _old_fish_prompt fish_prompt
@@ -23,7 +20,6 @@ function deactivate  -d "Exit virtual environment and return to normal shell env
     set -e VIRTUAL_ENV
     set -e VIRTUAL_ENV_PROMPT
     if test "$argv[1]" != "nondestructive"
-        # Self-destruct!
         functions -e deactivate
     end
 end
@@ -35,29 +31,20 @@ set -gx VIRTUAL_ENV /root/TestServerBitrix/venv
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
 
-# Unset PYTHONHOME if set.
 if set -q PYTHONHOME
     set -gx _OLD_VIRTUAL_PYTHONHOME $PYTHONHOME
     set -e PYTHONHOME
 end
 
 if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
-    # fish uses a function instead of an env var to generate the prompt.
-
-    # Save the current fish_prompt function as the function _old_fish_prompt.
     functions -c fish_prompt _old_fish_prompt
 
-    # With the original prompt function renamed, we can override with our own.
     function fish_prompt
-        # Save the return status of the last command.
         set -l old_status $status
 
-        # Output the venv prompt; color taken from the blue of the Python logo.
         printf "%s%s%s" (set_color 4B8BBE) '(venv) ' (set_color normal)
 
-        # Restore the return status of the previous command.
         echo "exit $old_status" | .
-        # Output the original/"old" prompt.
         _old_fish_prompt
     end
 
